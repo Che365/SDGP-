2fbd06adec8943eb0bf584c5a69ba796190663f4
Che365 <chethika.20210213@iit.ac.lk>
2024-03-07 13:38:06 +0530

    Finished Upto uploading photos

---
+++ app/src/main/java/com/example/sdgpfrontend/UploadPhotos.kt
@@ -0,0 +1,11 @@
+package com.example.sdgpfrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UploadPhotos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_photos)
+    }
+}
